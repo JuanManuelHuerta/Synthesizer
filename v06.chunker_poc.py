1e9692c12e16d4b51a554350d8f23fec3b23f4b7
JuanMHuerta <jmhuertany@gmail.com>
2020-06-13 20:54:02 -0400

    v06: polyphonic multi-thread sequential queues different wave shapes

--- v06.chunker_poc.py
+++ v06.chunker_poc.py
@@ -10,7 +10,7 @@ from scipy import signal
 from scipy import interpolate
 from numpy import *
 import threading,time, queue
-
+import itertools
 
 
 #https://people.csail.mit.edu/hubert/pyaudio/docs/
@@ -23,23 +23,30 @@ waves = {}
 
 def init_soundwaves():
     volume = 1.0     # range [0.0, 1.0]
-    duration = 1.0   # in seconds, may be float
-    decay=-0.000002
+    duration = 0.6   # in seconds, may be float
+    decay=-0.000001
     for i in range(22,122):
         note_mapper = (2.0**((i-69)/12.0))*440.0
         gain=np.exp(decay*np.arange(fs*duration))
-        samples = (volume*(1.0-gain)*gain*np.sin(2*np.pi*np.arange(fs*duration)*note_mapper/fs)).astype(np.float32)
+
+        #kernel=np.sign(np.sin(2*np.pi*np.arange(fs*duration)*note_mapper/fs))
+        #kernel=np.sign(np.sin(2*np.pi*np.arange(fs*duration)*note_mapper/fs))
+        kernel=signal.sawtooth(2*np.pi*np.arange(fs*duration)*note_mapper/fs)
+
+        samples = (volume*(1.0-gain)*gain* kernel ).astype(np.float32)
+
         print(len(samples))
         waves[i]=np.split(samples,int(chunks_per_second*duration))
 
-def play_audio():
+def play_audio(Qout):
     p = pyaudio.PyAudio()
     # open output stream
     ostream = p.open(format=pyaudio.paFloat32, channels=1, rate=int(fs),output=True,output_device_index=None)
     # play audio
     while ( 1 ):
         try:
-            data = my_queue.pop(0)
+            #data = my_queue.pop(0)
+            data = Qout.get()
         except:
             continue
         if data=="EOT" :
@@ -50,18 +57,35 @@ def play_audio():
             continue
     ostream.close()
 
-def play_note(note_value):
-    for chunk in waves[note_value]:
-        #stream.write(chunk)
-        my_queue.append(chunk)
+def execute_note(note_value,Qout):
+    list_a = waves[note_value]
+    list_b = []
+    list_c = []
+    while Qout.empty() is False:
+        try:
+            data = Qout.get()
+            list_b.append(data)
+        except:
+            continue
+    for item in itertools.zip_longest(list_a,list_b):
+        print("zipping")
+        if item[0] is None:
+            Qout.put(item[1])
+        elif item[1] is None:
+            Qout.put(item[0])
+        elif item[0] is not None and item[1] is not None:
+            Qout.put(item[0]+item[1])
+
+    #for chunk in list_a:
+    #    Qout.put(chunk)
     print("len of my queue",len(my_queue))
 
-def listen_midi():
+def listen_midi(Qout):
     print("Im here")
     for msg in mido.open_input():
         print(msg)
         if msg.type == 'note_on':
-            play_note(msg.note)
+            execute_note(msg.note,Qout)
     
 
 def main():
@@ -76,9 +100,9 @@ def main():
     stop_flag = threading.Event()
 
     # initialize threads
-    t_listen_midi = threading.Thread(target=listen_midi)
+    t_listen_midi = threading.Thread(target=listen_midi,args = (Qout,))
     #t_put_data = threading.Thread(target = put_data,   args = (Qout, ptrain, stop_flag  ))
-    t_play_audio = threading.Thread(target = play_audio)
+    t_play_audio = threading.Thread(target = play_audio, args = (Qout,))
     #t_signal_process = threading.Thread(target = signal_process, args = ( Qin, Qdata, pulse_a, Nseg, Nplot, fs, maxdist, temperature, functions, stop_flag))
     # start threads
     t_listen_midi.start()
